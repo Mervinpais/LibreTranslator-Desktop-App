4d1634de2ff79576a7b99ba4220009d4de2e0848
Mervinpaismakeswindows14 <mervinpaismakeswindows14@gmail.com>
2024-05-24 19:02:19 +0400

    Add project files.

---
+++ LTSAD_LibreTranslatorStandAloneDesktopApp/Form1.cs
@@ -0,0 +1,10 @@
+namespace LTSAD_LibreTranslatorStandAloneDesktopApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
